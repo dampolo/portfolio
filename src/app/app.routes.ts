ba0b14a14effc1ac91f6926febb35f9f12e47f5e
Damian Poloczek <damianpoloczek@gmail.com>
2024-07-24 23:15:17 +0200

    translation almost ready part 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { MainContentComponent } from './main-content/main-content.component';
 import { ImprintComponent } from './imprint/imprint.component';
 
 import { HeaderComponent } from './shared/components/header/header/header.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: MainContentComponent },
     { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
     { path: 'aside-main-nav', loadComponent: () => import('.//shared/components/header/header/header.component').then(m => m.HeaderComponent) }
 ];
