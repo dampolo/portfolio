60e2b8c38bc285414634e1349a28c8940bb3daa1
Damian Poloczek <damianpoloczek@gmail.com>
2024-08-29 08:52:55 +0200

    remove some unnecessary elements from app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
-import { Routes, PreloadAllModules, RouterModule, Router } from '@angular/router';
+import { Routes} from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { ImprintComponent } from './imprint/imprint.component';
-
-import { HeaderComponent } from './shared/components/header/header/header.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { SentMessageComponent } from './sent-message/sent-message.component';
 import { InProgressComponent } from './in-progress/in-progress.component';
@@ -13,7 +11,4 @@ export const routes: Routes = [
     { path: 'privacy-policy', component: PrivacyPolicyComponent },
     { path: 'sent-message-confirmation', component: SentMessageComponent },
     { path: 'in-progress-component', component: InProgressComponent },
-
-
-    { path: 'aside-main-nav', loadComponent: () => import('.//shared/components/header/header/header.component').then(m => m.HeaderComponent) }
 ];
