0547dfaeacb0505097643a05406c064c3132cad9
Damian Poloczek <damianpoloczek@gmail.com>
2025-03-10 19:13:43 +0100

    dabubble

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -16,47 +16,47 @@ export class PortfolioComponent {
   portfolio = [
     {
       id: 0,
+      image: "assets/img/da-bubble.png",
+      project_name: 'DaBubble',
+      language: 'Angular | HTML | CSS | TypeScript | Firebase',
+      description: 'MY_PROJECTS.PROJECT_1',
+      test: 'Live test',
+      test_link: 'https://dabubble.damianpoloczek.com/',
+      git_hub: 'GitHub',
+      link_git_hub: 'https://github.com/GalasMichal/DA_Bubble'
+    },
+    {
+      id: 1,
       image: "assets/img/join.png",
       project_name: 'Join',
       language: 'Java Script | HTML | CSS',
-      description: 'MY_PROJECTS.PROJECT_1',
+      description: 'MY_PROJECTS.PROJECT_2',
       test: 'Live test',
       test_link: 'https://join.damianpoloczek.com/',
       git_hub: 'GitHub',
       link_git_hub: 'https://github.com/dampolo/join'
     },
     {
-      id: 1,
+      id: 2,
       image: 'assets/img/pollo-loco.png',
       project_name: 'El Pollo loco',
       language: 'Java Script | HTML | CSS',
-      description: 'MY_PROJECTS.PROJECT_2',
+      description: 'MY_PROJECTS.PROJECT_3',
       test: 'Live test',
       test_link: 'https://elpolloloco.damianpoloczek.com/',
       git_hub: 'GitHub',
       link_git_hub: 'https://github.com/dampolo/ElPolloLoco'
     },
     {
-      id: 2,
+      id: 3,
       image: 'assets/img/pokemon.png',
       project_name: 'Pokemons',
       language: 'JavaScript | HTML | CSS | Bootstrap | API',
-      description: 'MY_PROJECTS.PROJECT_3',
+      description: 'MY_PROJECTS.PROJECT_4',
       test: 'Live test',
       test_link: 'https://pokemons.damianpoloczek.com/',
       git_hub: 'GitHub',
       link_git_hub: 'https://github.com/dampolo/PokeApi'
-    },
-    {
-      id: 3,
-      image: 'assets/img/simple-crm.png',
-      project_name: 'Simple CRM',
-      language: 'Angular | Firebase',
-      description: 'MY_PROJECTS.PROJECT_4',
-      test: 'Live test',
-      test_link: '/in-progress-component',
-      git_hub: 'GitHub',
-      link_git_hub: '/in-progress-component'
     },
   ]
 }
