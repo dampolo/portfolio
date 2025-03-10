983bdb0dc0ab68fe59245d3ca8be7f4cf662b05a
Damian Poloczek <damianpoloczek@gmail.com>
2025-03-10 19:46:26 +0100

    change single project to interface

--- src/app/main-content/portfolio/single-project/single-project.component.ts
+++ src/app/main-content/portfolio/single-project/single-project.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { SingleProject } from '../../../models/singleProject.model';
 
 @Component({
   selector: 'app-single-project',
@@ -11,15 +12,6 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class SingleProjectComponent {
   @Input() index: number = 0;
-  @Input() project = {
-    id: 0,
-    image: "assets/img/join.png",
-    project_name: 'Join',
-    language: 'Java Script | HTML | CSS',
-    description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-    test: 'Live test',
-    test_link: 'https://github.com/dampolo/ElPolloLoco',
-    git_hub: 'GitHub',
-    link_git_hub: 'https://github.com/dampolo/ElPolloLoco'
-  };
+  @Input() project!: SingleProject;
+
 }
---
+++ src/app/models/singleProject.model.ts
@@ -0,0 +1,11 @@
+export interface SingleProject {
+        id: number,
+        image: string,
+        project_name: string,
+        language: string,
+        description: string,
+        test: string,
+        test_link: string,
+        git_hub: string,
+        link_git_hub: string     
+}
