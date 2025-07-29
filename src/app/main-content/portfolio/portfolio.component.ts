d4b328430e32c7ebda19fce1e590592b1ff1d940
Damian Poloczek <damianpoloczek@gmail.com>
2025-07-29 22:31:36 +0200

    project updated

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -12,62 +12,73 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
-
+  
   portfolio = [
     {
       id: 0,
-      image: "assets/img/da-bubble.png",
-      project_name: 'DaBubble',
-      language: 'Angular | HTML | CSS | TypeScript | Firebase',
+      image: 'assets/img/quizapp.png',
+      project_name: 'Quizapp',
+      language: 'JavaScript | HTML | CSS | Bootstrap',
       description: 'MY_PROJECTS.PROJECT_1',
       test: 'Live test',
-      test_link: 'https://dabubble.damianpoloczek.com/',
+      test_link: 'https://quizapp.damianpoloczek.com/',
       git_hub: 'GitHub',
-      link_git_hub: 'https://github.com/dampolo/dabubble'
+      link_git_hub: 'https://github.com/dampolo/Quizapp'
     },
     {
       id: 1,
-      image: "assets/img/join.png",
-      project_name: 'Join',
-      language: 'Java Script | HTML | CSS',
-      description: 'MY_PROJECTS.PROJECT_2',
-      test: 'Live test',
-      test_link: 'https://join.damianpoloczek.com/',
-      git_hub: 'GitHub',
-      link_git_hub: 'https://github.com/dampolo/join'
-    },
-    {
-      id: 2,
       image: 'assets/img/pollo-loco.png',
       project_name: 'El Pollo loco',
       language: 'Java Script | HTML | CSS',
-      description: 'MY_PROJECTS.PROJECT_3',
+      description: 'MY_PROJECTS.PROJECT_2',
       test: 'Live test',
       test_link: 'https://elpolloloco.damianpoloczek.com/',
       git_hub: 'GitHub',
       link_git_hub: 'https://github.com/dampolo/ElPolloLoco'
     },
     {
-      id: 3,
+      id: 2,
       image: 'assets/img/pokemon.png',
       project_name: 'Pokemons',
       language: 'JavaScript | HTML | CSS | Bootstrap | API',
-      description: 'MY_PROJECTS.PROJECT_4',
+      description: 'MY_PROJECTS.PROJECT_3',
       test: 'Live test',
       test_link: 'https://pokemons.damianpoloczek.com/',
       git_hub: 'GitHub',
       link_git_hub: 'https://github.com/dampolo/PokeApi'
     },
     {
+      id: 3,
+      image: "assets/img/join.png",
+      project_name: 'Join',
+      language: 'Java Script | HTML | CSS',
+      description: 'MY_PROJECTS.PROJECT_4',
+      test: 'Live test',
+      test_link: 'https://join.damianpoloczek.com/',
+      git_hub: 'GitHub',
+      link_git_hub: 'https://github.com/dampolo/join'
+    },
+    {
       id: 4,
-      image: 'assets/img/quizapp.png',
-      project_name: 'Quizapp',
-      language: 'JavaScript | HTML | CSS | Bootstrap',
+      image: "assets/img/da-bubble.png",
+      project_name: 'DaBubble',
+      language: 'Angular | HTML | CSS | TypeScript | Firebase',
       description: 'MY_PROJECTS.PROJECT_5',
       test: 'Live test',
-      test_link: 'https://quizapp.damianpoloczek.com/',
+      test_link: 'https://dabubble.damianpoloczek.com/',
       git_hub: 'GitHub',
-      link_git_hub: 'https://github.com/dampolo/Quizapp'
+      link_git_hub: 'https://github.com/dampolo/dabubble'
+    },
+    {
+      id: 5,
+      image: "assets/img/startinkrypto.png",
+      project_name: 'Start in Krypto',
+      language: 'JavaScript | HTML | CSS',
+      description: 'MY_PROJECTS.PROJECT_6',
+      test: 'Live test',
+      test_link: 'https://www.startinkrypto.de/',
+      git_hub: 'GitHub',
+      link_git_hub: 'https://github.com/dampolo/startinkrypto'
     },
   ]
 }
