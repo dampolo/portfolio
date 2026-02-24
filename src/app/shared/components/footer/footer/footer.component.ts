22f665a3694a9fb4c74364144b69fafe160d8b99
Damian Poloczek <damianpoloczek@gmail.com>
2026-02-24 23:10:32 +0100

    year date

--- src/app/shared/components/footer/footer/footer.component.ts
+++ src/app/shared/components/footer/footer/footer.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { ContactComponent } from '../../../../main-content/contact/contact.component';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [ContactComponent, TranslateModule],
+  imports: [TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  currentYear = new Date().getFullYear();
 }
