eb395f9f37978ea404f1a3d7de9dcdfb681ebdf7
Damian Poloczek <damianpoloczek@gmail.com>
2026-08-27 14:16:59 +0200

    remove debugger

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -46,7 +46,6 @@ export class ContactComponent {
   }
 
   onSubmit(ngForm: NgForm) {
-    debugger
     if (!this.checkboxState) {
       this.checkboxChecked = true;
     } else if (ngForm.submitted && ngForm.form.valid) {
